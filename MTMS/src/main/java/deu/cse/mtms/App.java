391d337fd52d34035135cfcb5f45fa6987b3d6c9
pocket0207 <pocpoc0202@naver.com>
2020-05-10 10:37:36 +0900

    정대겸 테스트 주석

--- MTMS/src/main/java/deu/cse/mtms/App.java
+++ MTMS/src/main/java/deu/cse/mtms/App.java
@@ -30,7 +30,7 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
-
+    /// 정대겸 테스트 파일
     public static void main(String[] args) {
         launch();
     }
