b86fc629a4ddb5232e3eb1c58a29082ed702a485
HyunJin-Kim330 <61076055+HyunJin-Kim330@users.noreply.github.com>
2020-06-01 14:03:07 +0900

    x

--- MTMS/src/main/java/movie/manage/EnrollMovie.java
+++ MTMS/src/main/java/movie/manage/EnrollMovie.java
@@ -2,17 +2,23 @@ package movie.manage;
 
 import team3.mtms.DatabaseConnector;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class EnrollMovie {
     
     Scanner sc = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private String query;
     ResultSet rs = null;
     
     
-    public Movie enrollMovie(){
+    
+    public Movie enrollMovie() throws IOException{
+        
         String tmpId;
         String tmpTitle;
         String tmpCategory;
@@ -25,11 +31,12 @@ public class EnrollMovie {
         String rating1 ="A,B,C,D";
         String [] rating2 =rating1.split(",");
         
-        tmpId = "100";
+        tmpId = "105";
         
         System.out.println("영화를 등록합니다.");
         System.out.println("영화 제목을 입력해주세요");
-        tmpTitle = sc.nextLine();
+        tmpTitle = br.readLine();
+        System.out.println(tmpTitle);
         System.out.println("영화 장르를 선택해주세요\n"  // 사용자 입력시 오타가나면 곤란하기 때문에 숫자로 선택
                 + "1. 코미디  2. 액션  3. 스릴러  4. SF  5. 판타지  6. 로맨스");
         select = sc.nextInt();
